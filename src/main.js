b58a59bb5f18e0b26c1b0ab6d5ce13e0f67ef995
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2023-02-14 15:24:16 +0200

    Add the vee-validate and registered it globally

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import VeeValidatePlugin from './includes/validation'
 
 import "./assets/css/main.css";
 
 const app = createApp(App);
 
+
+app.use(VeeValidatePlugin)
 app.use(createPinia());
 app.use(router);
 
