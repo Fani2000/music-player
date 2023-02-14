904be397d3c6b7afa0d9bd8abe480dedbde5cffe
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2023-02-14 16:52:01 +0200

    Completed validation for the user in the app for both login and register

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -9,6 +9,55 @@ const tab = ref("login");
 const { isAuthModal } = storeToRefs(authStore);
 
 const { toggleAuthModal } = authStore;
+
+const registerSchema = {
+  name: "required|min:3|max:100|alphaSpaces",
+  email: "required|email|min:10|max:100",
+  age: "required|minVal:18|maxVal:100",
+  password: "required|min:4",
+  confirm_password: "required|confirmed:@password",
+  country: "required|country_not_allowed:Antarctica",
+  tos: "tos",
+};
+
+const loginSchema = {
+  email: "required|email|min:10|max:100",
+  password: "required|min:4",
+};
+
+const reg_in_submission = ref(false);
+const reg_show_alert = ref(false);
+const reg_alert_variant = ref("bg-blue-500");
+const reg_alert_msg = ref("Please wait! Your account is being created.");
+
+const log_in_submission = ref(false);
+const log_show_alert = ref(false);
+const log_alert_variant = ref("bg-blue-500");
+const log_alert_msg = ref("Please wait! You are logging in.");
+
+const handleLoginUser = (formData) => {
+  console.log(formData);
+  log_show_alert.value = true;
+  log_in_submission.value = true;
+  log_alert_variant.value = "bg-blue-500";
+  log_alert_msg.value = "Please wait! Your account is being created.";
+
+
+  log_alert_variant.value = "bg-green-500";
+  log_alert_msg.value = "Success! You have successfully logged in.";
+};
+
+const handleRegisterUser = (formData) => {
+  console.log(formData);
+  reg_show_alert.value = true;
+  reg_in_submission.value = true;
+  reg_alert_variant.value = "bg-blue-500";
+  reg_alert_msg.value = "Please wait! Your account is being created.";
+
+
+  reg_alert_variant.value = "bg-green-500";
+  reg_alert_msg.value = "Success! Your account has been created.";
+};
 </script>
 <template>
   <!-- prettier-ignore -->
@@ -59,95 +108,131 @@ const { toggleAuthModal } = authStore;
             </li>
           </ul>
 
-          <form v-show="tab === 'login'">
+          <div class="text-white text-center font-bold p-4 rounded mb-4" v-if="log_show_alert"
+          :class="log_alert_variant"
+          >
+          {{ log_alert_msg }}
+        </div>
+          <!-- LOGIN FORM -->
+          <vee-form v-show="tab === 'login'" @submit="handleLoginUser" :validation-schema="loginSchema">
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <error-message class="text-red-600" name="email" />
             </div>
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <error-message class="text-red-600" name="password" />
             </div>
             <button
               type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              :disabled="log_in_submission"
             >
               Submit
             </button>
-          </form>
+          </vee-form>
 
-          <form v-show="tab === 'register'">
+          <div class="text-white text-center font-bold p-4 rounded mb-4" v-if="reg_show_alert"
+          :class="reg_alert_variant"
+          >
+          {{ reg_alert_msg }}
+        </div>
+
+          <!-- REGISTRATION FORM -->
+          <vee-form @submit="handleRegisterUser" v-show="tab === 'register'" :validation-schema="registerSchema">
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field 
                 type="text"
+                name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <error-message class="text-red-600" name="name" />
             </div>
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <error-message class="text-red-600" name="email" />
             </div>
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input
+              <vee-field
                 type="number"
+                name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
+              <error-message class="text-red-600" name="age" />
             </div>
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
+              name="password"
                 type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <error-message class="text-red-600" name="password" />
             </div>
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="confirm_password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
+              <error-message class="text-red-600" name="confirm_password" />
             </div>
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <vee-field as="select"
+                name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-              >
+                >
+                <option value="RSA">South Africa</option>
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+                <option value="Antarctica">Antarctica</option>
+              </vee-field>
+              <error-message class="text-red-600" name="country" />
             </div>
             <div class="mb-3 pl-6">
-              <input
+              <vee-field
+                name="tos"
+                value="1"
                 type="checkbox"
                 class="w-4 h-4 float-left -ml-6 mt-1 rounded"
               />
               <label class="inline-block">Accept terms of service</label>
             </div>
+            <error-message class="text-red-600" name="tos" />
             <button
               type="submit"
+              :disabled="reg_in_submission"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,9 +1,54 @@
-import { Form as VeeForm, Field as VeeField } from "vee-validate";
+// prettier-ignore
+import { Form as VeeForm, Field as VeeField, ErrorMessage, defineRule, configure } from "vee-validate";
+// prettier-ignore
+import { required, email, min, max, alpha_spaces as alphaSpaces, min_value as minVal, max_value as maxVal, confirmed, not_one_of as excluded } from "@vee-validate/rules";
 
 export default {
   install(app) {
     console.log("RUNNING THE VEE VALIDATE PLUGIN");
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
+
+    defineRule("required", required);
+    defineRule("tos", required);
+    defineRule("email", email);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alphaSpaces", alphaSpaces);
+    defineRule("minVal", minVal);
+    defineRule("maxVal", maxVal);
+    defineRule("confirmed", confirmed);
+    defineRule("excluded", excluded);
+    defineRule("country_not_allowed", excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required.`,
+          min: `The field ${ctx.field} is too short.`,
+          max: `The field ${ctx.field} is too max.`,
+          alphaSpaces: `The field ${ctx.field} may only contain alphabetical characters and alpha spaces.`,
+          email: `The field ${ctx.field} must be a valid email.`,
+          maxVal: `The field ${ctx.field} is too hight.`,
+          minVal: `The field ${ctx.field} is too low.`,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}`,
+          confirmed: `The passwords don't match.`,
+          tos: "You must accept the Terms of Service.",
+          country_not_allowed:
+            "Due to restrictions, we do not accept users from this location.",
+        };
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : "The field " + ctx.field + " is invalid.";
+
+        return message;
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
